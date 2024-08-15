b72598ca0e4dfe75a67babf22e96417999314929
guidomora <guidomorabito161@hotmail.com>
2024-08-15 16:16:57 -0300

    ejercicio

---
+++ firstJava/src/Ala.java
@@ -0,0 +1,13 @@
+public class Ala {
+
+    // Atributos
+    public String nombre;
+    public String color;
+    public int envergadura;
+    public String material;
+
+    // Metodos
+    public String flap(){
+        return "flap funcionando" + " " + nombre;
+    }
+}
--- firstJava/src/App.java
+++ firstJava/src/App.java
@@ -1,8 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int numero = 10;
-        String nombre = "Guido";
-        System.out.println(numero);
-        System.out.println(nombre);
+        Ala ala1 = new Ala();
+
+        ala1.nombre = "Ala 1";
+        ala1.color = "Blanco";
+        ala1.envergadura = 40;
+        ala1.material = "Metal";
+        
+        System.out.println(ala1.flap());
+
+        Ala ala2 = new Ala();
+
+        ala2.nombre = "Ala 2";
+        ala2.color = "Negro";
+        ala2.envergadura = 40;
+        ala2.material = "Metal";
+
+        System.out.println(ala2.flap());
+    
     }
 }
